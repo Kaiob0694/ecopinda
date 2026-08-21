6d83ea263a0f6c14be80eed76364a9bfd03e5942
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-08-21 11:56:50 -0300

    [GRANDE MUDANÇA]

--- pages/hoteis/read.php
+++ pages/hoteis/read.php
@@ -1,9 +1,7 @@
-<?php 
-
+<?php
 
 error_reporting(E_ALL);
-ini_set('display_errors',1); 
-
+ini_set('display_errors', 1);
 
 require_once "../../classes/hoteis.php";
 
@@ -12,97 +10,164 @@ $dados = $hotel->listar();
 
 include "../../includes/header.php";
 include "../../includes/head.php";
-
 ?>
 
-<div class="container">
+<link rel="stylesheet" href="/ecopinda/assets/css/hotel.css">
 
-    <div class="card">
+<div class="hoteis-container">
 
-        <h2>Lista de Hotéis</h2>
+    <div class="hoteis-painel">
 
-        <a href="create.php" class="botao-novo">
-            Cadastrar Novo Hotel
-        </a>
+        <div class="hoteis-topo">
 
-        <br><br>
+            <h2 class="hoteis-titulo">
+                Lista de Hotéis
+            </h2>
 
-
-    <table border="1">
-
-        <tr>
-
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Endereço</th>
-            <th>Cidade</th>
-            <th>CEP</th>
-            <th>Telefone</th>
-            <th>Email</th>
-            <th>Quantidade de Quartos</th>
-            <th>Possui Wi-Fi</th>
-            <th>Possui Estacionamento</th>
-            <th>Data de Cadastro</th>
-
-            </tr>
-
-    <?php foreach ($dados as $linha): ?>
-
-        <tr>
-
-            <td><?= $linha['id_hoteis'] ?></td>
-            <td><?= $linha['nome'] ?></td>
-            <td><?= $linha['endereco'] ?></td>
-            <td><?= $linha['cidade'] ?></td>
-            <td><?= $linha['cep'] ?></td>
-            <td><?= $linha['telefone'] ?></td>
-            <td><?= $linha['email'] ?></td>
-            <td><?= $linha['quantidade_quartos'] ?></td>
-            <td><?= $linha['possui_wifi'] ? 'Sim' : 'Não' ?></td>
-            <td><?= $linha['possui_estacionamento'] ? 'Sim' : 'Não' ?></td>
-            <td><?= $linha['data_cadastro'] ?></td>
-
-           <td>
-
-         <a class="editar"
-         href="editar.php?id=<?= $linha['id_hoteis'] ?>">
-         Editar
+            <a href="create.php" class="botao-novo">
+                Cadastrar Novo Hotel
             </a>
 
-         <a class="excluir"
-          href="excluir.php?id=<?= $linha['id_hoteis'] ?>"
-          onclick="return confirm('Deseja realmente excluir este hotel?')">
-         Excluir
-            </a>
+        </div>
 
-</td>
+        <div class="tabela-wrapper">
 
-                </tr>
+            <table class="tabela-hoteis">
 
-            <?php endforeach; ?>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Endereço</th>
+                        <th>Cidade</th>
+                        <th>CEP</th>
+                        <th>Telefone</th>
+                        <th>Email</th>
+                        <th>Quantidade de Quartos</th>
+                        <th>Possui Wi-Fi</th>
+                        <th>Possui Estacionamento</th>
+                        <th>Data de Cadastro</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
 
-        </table>
+                <tbody>
+
+                    <?php foreach ($dados as $linha): ?>
+
+                        <tr>
+
+                            <td>
+                                <?= $linha['id_hoteis'] ?>
+                            </td>
+
+                            <td>
+                                <?= $linha['nome'] ?>
+                            </td>
+
+                            <td>
+                                <?= $linha['endereco'] ?>
+                            </td>
+
+                            <td>
+                                <?= $linha['cidade'] ?>
+                            </td>
+
+                            <td>
+                                <?= $linha['cep'] ?>
+                            </td>
+
+                            <td>
+                                <?= $linha['telefone'] ?>
+                            </td>
+
+                            <td>
+                                <?= $linha['email'] ?>
+                            </td>
+
+                            <td>
+                                <?= $linha['quantidade_quartos'] ?>
+                            </td>
+
+                            <td>
+
+                                <?php if ($linha['possui_wifi']): ?>
+
+                                    <span class="status-sim">
+                                        Sim
+                                    </span>
+
+                                <?php else: ?>
+
+                                    <span class="status-nao">
+                                        Não
+                                    </span>
+
+                                <?php endif; ?>
+
+                            </td>
+
+                            <td>
+
+                                <?php if ($linha['possui_estacionamento']): ?>
+
+                                    <span class="status-sim">
+                                        Sim
+                                    </span>
+
+                                <?php else: ?>
+
+                                    <span class="status-nao">
+                                        Não
+                                    </span>
+
+                                <?php endif; ?>
+
+                            </td>
+
+                            <td>
+                                <?= $linha['data_cadastro'] ?>
+                            </td>
+
+                            <td>
+
+                                <div class="acoes">
+
+                                    <a
+                                        class="editar"
+                                        href="editar.php?id=<?= $linha['id_hoteis'] ?>"
+                                    >
+                                        Editar
+                                    </a>
+
+                                    <a
+                                        class="excluir"
+                                        href="excluir.php?id=<?= $linha['id_hoteis'] ?>"
+                                        onclick="return confirm('Deseja realmente excluir este hotel?')"
+                                    >
+                                        Excluir
+                                    </a>
+
+                                </div>
+
+                            </td>
+
+                        </tr>
+
+                    <?php endforeach; ?>
+
+                </tbody>
+
+            </table>
+
+        </div>
+
     </div>
 
-    <?php
+</div>
 
-    include "../../includes/footer.php";
+<?php
 
+include "../../includes/footer.php";
 
-
-
-
-
-
-
-         
-
-
-
-
-
-
-
-         
-
-     
+?>
