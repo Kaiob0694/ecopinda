1be01e9574d83b028a34f4120d59f6131321222b
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-02 09:32:11 -0300

    [FIX]LOCALIZANDO BANDO DE DADOS

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'includes/head.php'; ?>
 <?php include 'includes/header.php'; ?>
-<link rel="stylesheet" href="/ecopinda/style.css">
+<link rel="stylesheet" href="/style.css">
  
     <section class="hero">
         <video autoplay muted loop playsinline class="video-bg">
