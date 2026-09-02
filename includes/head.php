ca9d377b999124a683d55ae6427171134e2566df
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-02 09:43:49 -0300

    [FIX] Corrigindo caminho de HEAD

--- includes/head.php
+++ includes/head.php
@@ -10,7 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pinda Eco</title>
-    <link rel="stylesheet" href="/ecopinda/assets/css/style_header.css">
+    <link rel="stylesheet" href="/../assets/css/style_header.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
