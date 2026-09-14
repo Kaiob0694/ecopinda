bda59f21c7ef6ed17014d76d29d7e33e3938f548
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-14 10:30:13 -0300

    [FIX] caminho do conexão

--- src/cadastrar.php
+++ src/cadastrar.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require "conexao.php";
+require_once(__DIR__ . "/../config/conexao.php");
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: ../pages/cadastro.php");
