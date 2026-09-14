bb357ed9ada0d86c6552c2f1303f1f9c5b2465a1
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-14 11:09:47 -0300

    [FIX] Conexao.php

--- src/cadastrar.php
+++ src/cadastrar.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require "/config/conexao.php";
+require "../config/conexao.php";
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header("Location: ../pages/cadastro.php");
