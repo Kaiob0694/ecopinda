b687469338dfcb010d1d9cba85a95d5c14159f4c
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-14 09:14:54 -0300

    [FIX] trocando erro de caminho

--- pages/hoteis/read.php
+++ pages/hoteis/read.php
@@ -139,7 +139,7 @@ include "../../includes/head.php";
                                     <?php foreach ($fotosHotel as $indice => $foto): ?>
 
                                         <img class="hotel-imagem <?= $indice === 0 ? 'ativa' : '' ?>"
-                                            src="<?= $baseUrl ?>assets/uploads/hoteis/<?= htmlspecialchars($foto['caminho']) ?>"
+                                            src="<?= $baseUrl ?>/assets/uploads/hoteis/<?= htmlspecialchars($foto['caminho']) ?>"
                                             alt="<?= htmlspecialchars($linha['nome']) ?>">
 
                                     <?php endforeach; ?>
