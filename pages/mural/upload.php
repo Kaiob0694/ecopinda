f687e6148eaa7f32dd58c4f15636ff722b71f8d3
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-15 13:52:00 -0300

    Corrigindo Upload das fotos

--- pages/mural/upload.php
+++ pages/mural/upload.php
@@ -1,9 +1,9 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 
 require_once "../../config/conexao.php";
 require_once "../../classes/mural_fotos.php";
