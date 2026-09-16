0def5bce41ac3e50ca4cf7c4fed5f92317573382
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-16 11:09:08 -0300

    Corrigindo Turisticos

--- pages/turismo/create.php
+++ pages/turismo/create.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 require_once "../../classes/turismo.php";
 require_once "../../includes/upload_fotos_turismo.php";
 
