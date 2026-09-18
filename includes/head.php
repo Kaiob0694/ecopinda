2a0d29ebaeb1fcedc8ca179ad75713240cd4ef9b
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-17 23:11:04 -0300

    Tela de carregamento

--- includes/head.php
+++ includes/head.php
@@ -11,6 +11,7 @@ $baseUrl = 'https://pindaeco.rf.gd';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pinda Eco</title>
+    <link rel="stylesheet" href="/../assets/css/preloader.css">
     <link rel="stylesheet" href="<?= $baseUrl ?>/assets/css/style_header.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
@@ -18,3 +19,4 @@ $baseUrl = 'https://pindaeco.rf.gd';
 </head>
 
 <body>
+    <?php include('preloader.html'); ?>
