04f04223d4c4b0fd9d80546228ec29d2b6e33f4e
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-17 23:14:44 -0300

    Tela de carregamento

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,10 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/gsap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/ScrollTrigger.min.js"></script>
-    <script src="script.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.13.0/ScrollTrigger.min.js"></script>
+<script src="script.js"></script>
 <script src="<?= $baseUrl ?>/assets/js/header-gsap.js"></script>
 <script src="<?= $baseUrl ?>/assets/js/preloader-gsap.js"></script>
 
-
-    <script async src="//www.instagram.com/embed.js"></script>
+<script async src="//www.instagram.com/embed.js"></script>
 
 </body>
-
 </html>
