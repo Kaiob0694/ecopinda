b4cfd1cb07ac9b4ecba5a4eb08eb30326560b711
Kaiob0694 <kaiobarbosa0694@gmail.com>
2026-09-18 09:19:49 -0300

    Implementação Calendario

--- includes/header.php
+++ includes/header.php
@@ -195,7 +195,7 @@ if (!function_exists('iniciaisHeader')) {
             <a href="/../index.php">
                 Loja de Recompensas
             </a>
-            <a href="/../pages/calendario.php">
+            <a href="/../pages/calendario/calendario.php">
                 Calendario
             </a>
 
